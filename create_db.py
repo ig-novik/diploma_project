08e277412537e5422b5023751ec64e39fc1ebe06
ig-novik <60872905+ig-novik@users.noreply.github.com>
2020-03-16 09:47:24 +0300

    Add files via upload

--- create_db.py
+++ create_db.py
@@ -1,3 +1,10 @@
 from webapp import db, create_app
+from sqlalchemy import create_engine
+from webapp.config import SQLALCHEMY_DATABASE_URI
 
 db.create_all(app=create_app())
+
+#создаём представление (вьюху) с нужным набором данных
+e = create_engine(SQLALCHEMY_DATABASE_URI)
+e.execute('''CREATE VIEW ads_img AS
+select ads.id as ad_id, ads.title as ad_title, ads.url as ad_url, (select img.src from img where img.ad_id = ads.id) as img_src, ads.published as ad_date from ads''')
