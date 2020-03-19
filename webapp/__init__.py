db4b8704c5884ddd6013a93cf7bafe5db0f3245f
Igor Novik <ig_novik@mail.ru>
2020-03-19 09:39:37 +0300

    12.03.2020

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+from webapp.forms import LoginForm
 from webapp.model import db, Ads, Img
 from webapp.weather import weather_by_city
 from webapp.config import SQLALCHEMY_DATABASE_URI
@@ -17,5 +18,11 @@ def create_app():
         ads_list = Ads.query.order_by(Ads.published.desc()).all()
 
         return render_template('index.html', page_title=page_title, weather=weather_, ads_list=ads_list)
+
+    @app.route('/login')
+    def login():
+        title = "Авторизация"
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
 
     return app
--- webapp/model.py
+++ webapp/model.py
@@ -3,17 +3,39 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Ads(db.Model):
+
     id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True, nullable=False)
     title = db.Column(db.String, nullable=False)
     url = db.Column(db.String, unique=True, nullable=False)
     published = db.Column(db.DateTime, nullable=False)
     images = db.relationship("Img", backref="img_src")
-
+    img_ad_id = None
     @property
     def first_image_src(self):
         if self.images:
             return self.images[0].src
         return ""
+
+    @property
+    def all_ads_images_src(self):
+        if self.images:
+            return [img.src for img in self.images]
+        return []
+
+
+
+    @property
+    def all_ad_images_src(self):
+        print(f'номер объявления  {self.img_ad_id}')
+        if self.id == self.img_ad_id:
+            if self.images:
+                return [img.src for img in self.images]
+            return []
+        return []
+
+    @all_ad_images_src.setter
+    def all_ad_images_src(self, value):
+        self.img_ad_id = value
 
     def __repr__(self):
         return 'Ads {} {}>'.format(self.title, self.url)
