1be243110929c74bc4a098da5a619f13514422aa
ig-novik <60872905+ig-novik@users.noreply.github.com>
2020-03-23 11:38:32 +0300

    Add files via upload

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 
-from webapp.model import db, Ads, Img
+from webapp.forms import LoginForm
+from webapp.model import db, Ads, Img, User
 from webapp.weather import weather_by_city
 from webapp.config import SQLALCHEMY_DATABASE_URI
 
@@ -10,6 +12,14 @@ def create_app():
     app.config.from_pyfile("config.py")
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
     @app.route('/')
     def index():
         page_title = "Продажа пресмыкающихся"
@@ -17,5 +27,41 @@ def create_app():
         ads_list = Ads.query.order_by(Ads.published.desc()).all()
 
         return render_template('index.html', page_title=page_title, weather=weather_, ads_list=ads_list)
+
+    @app.route('/login')
+    def login():
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+        title = "Авторизация"
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+
+    @app.route('/process-login', methods=['POST'])
+    def process_login():
+        form = LoginForm()
+
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('Вы вошли на сайт')
+                return redirect(url_for('index'))
+
+        flash('Неправильное имя пользователя или пароль')
+        return redirect(url_for('login'))
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('Вы успешно разлогинились')
+        return redirect(url_for('index'))
+
+    @app.route('/admin')
+    @login_required
+    def admin_index():
+        if current_user.is_admin:
+            return  'Привет админ!'
+        else:
+            return 'Ты не админ.'
 
     return app
--- webapp/model.py
+++ webapp/model.py
@@ -1,6 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
+
 
 class Ads(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True, nullable=False)
@@ -8,12 +11,29 @@ class Ads(db.Model):
     url = db.Column(db.String, unique=True, nullable=False)
     published = db.Column(db.DateTime, nullable=False)
     images = db.relationship("Img", backref="img_src")
+    img_ad_id = None
 
     @property
     def first_image_src(self):
         if self.images:
             return self.images[0].src
         return ""
+
+    @property
+    def all_ads_images_src(self):
+        if self.images:
+            return [img.src for img in self.images]
+        return []
+
+    @staticmethod
+    def img_src_for_ad_id(id_):
+
+        res = []
+        for ad in Ads.query.all():
+            for im in ad.images:
+                if im.ad_id == id_:
+                    res.append(im.src)
+        return res
 
     def __repr__(self):
         return 'Ads {} {}>'.format(self.title, self.url)
@@ -27,3 +47,23 @@ class Img(db.Model):
 
     def __repr__(self):
         return 'Image {} {}>'.format(self.alt, self.src)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password = db.Column(db.String(128))
+    role = db.Column(db.String(10), index=True)
+
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User name = {}, id = {}>'.format(self.username, self.id)
