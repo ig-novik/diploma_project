0fe4a7db62b9e9def0c869e6f9d199c6fcb03587
Igor Novik <ig_novik@mail.ru>
2020-03-24 21:19:52 +0300

    24.03.2020

---
+++ webapp/config.py
@@ -0,0 +1,11 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+WEATHER_DEFAULT_CITY = "Moscow,Russia"
+WEATHER_API_KEY = "ad4d399064a146bd8f4140022200303"
+WEATHER_URL = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+POSTS_PER_PAGE = 15
+
+SECRET_KEY = "cfasd893248^&%42342ASDCASCLd5%__fgs&54"
