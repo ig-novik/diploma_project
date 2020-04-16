0f950d39ab6221965677a9a08734d790cce29724
Igor Novik <ig_novik@mail.ru>
2020-04-16 21:13:25 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	html_parsing_tst.py
    #	webapp/static/style.css

--- webapp/ads/views.py
+++ webapp/ads/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, current_app, request, render_template
+from flask import abort, Blueprint, current_app, request, render_template
 
 from webapp.ads.models import Ads
 
@@ -14,6 +14,16 @@ def index(page=1):
         page = 1
     print(f'page = {pg}')
     page_title = "Продажа рептилий и террариумов в России"
-    ads_list = Ads.query.order_by(Ads.published.desc()).paginate(page, current_app.config["POSTS_PER_PAGE"], False)
+    ads_list = Ads.query.filter(Ads.text.isnot(None)).order_by(Ads.published.desc()).paginate(page, current_app.config["POSTS_PER_PAGE"], False)
 
     return render_template('ads/index.html', page_title=page_title, ads_list=ads_list)
+
+
+@blueprint.route('/ads/<int:ads_id>')
+def single_ad(ads_id):
+    my_ad = Ads.query.filter(Ads.id == ads_id).first()
+
+    if not my_ad:
+        abort(404)
+
+    return render_template('ads/single_ad.html', page_title=my_ad.title, text=my_ad.text)
